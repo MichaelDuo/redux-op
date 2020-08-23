3ea3d200977d24b083cae1d3533d37dd05a6ab21
MichaelDuo <michaeldong21@gmail.com>
2020-08-23 15:02:14 -0700

    Implement combine_op

---
+++ combineResources.ts
@@ -0,0 +1,33 @@
+import ReduxOP from './ReduxOP';
+import {Reducer, combineReducers, ReducersMapObject} from 'redux';
+import {mapValues} from 'lodash';
+
+type OPMapObject<S> = {[K in keyof S]: ReduxOP<S[K]>};
+
+export class CombinedOPs<S> {
+	private opMap: OPMapObject<S>;
+	constructor(opMap: OPMapObject<S>) {
+		this.opMap = opMap;
+	}
+
+	getRootReducer<SR>(
+		reducersMap?: ReducersMapObject<SR>
+	): Reducer<SR & S, any> {
+		const opRootReducer = (mapValues(
+			this.opMap,
+			(op) => op.reducer
+		) as unknown) as ReducersMapObject<S>;
+
+		const a = ({
+			...reducersMap,
+			...opRootReducer,
+		} as unknown) as ReducersMapObject<SR & S>;
+
+		const t = combineReducers(a);
+
+		return t;
+	}
+}
+
+export default <S>(opsMap: OPMapObject<S>): CombinedOPs<S> =>
+	new CombinedOPs(opsMap);
---
+++ index.ts
@@ -0,0 +1,1 @@
+export {default as ReduxOP} from './ReduxOP';
